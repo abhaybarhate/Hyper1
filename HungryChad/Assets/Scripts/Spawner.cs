ccb3b9d32e6af891dbfcd36c719bd45113e9bf3b
abhaybarhate <abhaybarhate5@gmail.com>
2022-03-19 09:09:31 +0530

    dx fhvhcf tu

--- HungryChad/Assets/Scripts/Spawner.cs
+++ HungryChad/Assets/Scripts/Spawner.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Spawner : MonoBehaviour{
 
     [SerializeField] float spawnDuration = 5f;
-    [SerializeField] float spawnDelay = 20f;
+    [SerializeField] float spawnDelay = 10f;
     [SerializeField] List<Item> spawnItems = new List<Item>();
     bool isStop = false;
+
     //[SerializeField] Item[] ItemsList = new Item[0];
 
     Vector3 spawnLocation;
@@ -28,9 +29,17 @@ public class Spawner : MonoBehaviour{
     IEnumerator SpawnItem() {
         while(isStop) {
             int randomItemCount = Random.Range(0, spawnItems.Count);
-            spawnItems[0].Spawn(DetermineSpawnLocation());
-            spawnItems[0].ItemPrefab.transform.localScale = new Vector3(10,10,10);
+            spawnItems[randomItemCount].Spawn(DetermineSpawnLocation());
+            StartCoroutine(DestroyItem(spawnItems[randomItemCount]));
             yield return new WaitForSeconds(spawnDelay);
+            
+        }
+    }
+
+    IEnumerator DestroyItem(Item item) {
+        while(true) {
+            item.ItemPrefab.SetActive(false);
+            yield return new WaitForSeconds(spawnDuration);
         }
     }
 
